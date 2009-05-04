efbfb743856ad07b97252c0aeb68cb5dead6d4a0
Leandro Perrotta <leandro_perrotta@hotmail.com>
2009-05-04 12:37:53 +0000

    *commitada novas sources, baseado no SVN Trunk :~~
    git-svn-id: svn://darghos.com.br/darghos/gameserver/trunk/svn@684 5021ead5-42e4-5646-a3ec-22fc1710fdc7

---
+++ outputmessage.cpp
@@ -0,0 +1,243 @@
+//////////////////////////////////////////////////////////////////////
+// OpenTibia - an opensource roleplaying game
+//////////////////////////////////////////////////////////////////////
+//
+//////////////////////////////////////////////////////////////////////
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License
+// as published by the Free Software Foundation; either version 2
+// of the License, or (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software Foundation,
+// Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+//////////////////////////////////////////////////////////////////////
+#include "otpch.h"
+
+#include "outputmessage.h"
+#include "connection.h"
+#include "protocol.h"
+#include "scheduler.h"
+
+extern Dispatcher g_dispatcher;
+
+OutputMessage::OutputMessage()
+{
+	freeMessage();
+}
+
+//*********** OutputMessagePool ****************
+
+OutputMessagePool::OutputMessagePool()
+{
+	for(uint32_t i = 0; i < OUTPUT_POOL_SIZE; ++i){
+		OutputMessage* msg = new OutputMessage();
+		m_outputMessages.push_back(msg);
+#ifdef __TRACK_NETWORK__
+		m_allOutputMessages.push_back(msg);
+#endif
+	}
+	m_frameTime = OTSYS_TIME();
+}
+
+void OutputMessagePool::startExecutionFrame()
+{
+	//boost::recursive_mutex::scoped_lock lockClass(m_outputPoolLock);
+	m_frameTime = OTSYS_TIME();
+	m_isOpen = true;
+}
+
+OutputMessagePool::~OutputMessagePool()
+{
+	InternalOutputMessageList::iterator it;
+	for(it = m_outputMessages.begin(); it != m_outputMessages.end(); ++it){
+		delete *it;
+	}
+	m_outputMessages.clear();
+}
+
+void OutputMessagePool::send(OutputMessage_ptr msg)
+{
+	m_outputPoolLock.lock();
+	OutputMessage::OutputMessageState state = msg->getState();
+	m_outputPoolLock.unlock();
+
+	if(state == OutputMessage::STATE_ALLOCATED_NO_AUTOSEND){
+		#ifdef __DEBUG_NET_DETAIL__
+		std::cout << "Sending message - SINGLE" << std::endl;
+		#endif
+
+		if(msg->getConnection()){
+			if(!msg->getConnection()->send(msg)){
+				// Send only fails when connection is closing (or in error state)
+				// This call will free the message
+				msg->getProtocol()->onSendMessage(msg);
+			}
+		}
+		else{
+			#ifdef __DEBUG_NET__
+			std::cout << "Error: [OutputMessagePool::send] NULL connection." << std::endl;
+			#endif
+		}
+	}
+	else{
+		#ifdef __DEBUG_NET__
+		std::cout << "Warning: [OutputMessagePool::send] State != STATE_ALLOCATED_NO_AUTOSEND" << std::endl;
+		#endif
+	}
+}
+
+void OutputMessagePool::sendAll()
+{
+	boost::recursive_mutex::scoped_lock lockClass(m_outputPoolLock);
+	OutputMessageMessageList::iterator it;
+	for(it = m_autoSendOutputMessages.begin(); it != m_autoSendOutputMessages.end(); ){
+		OutputMessage_ptr omsg = *it;
+		#ifdef __NO_PLAYER_SENDBUFFER__
+		//use this define only for debugging
+		bool v = 1;
+		#else
+		//It will send only messages bigger then 1 kb or with a lifetime greater than 10 ms
+		bool v = omsg->getMessageLength() > 1024 || (m_frameTime - omsg->getFrame() > 10);
+		#endif
+		if(v){
+			#ifdef __DEBUG_NET_DETAIL__
+			std::cout << "Sending message - ALL" << std::endl;
+			#endif
+
+			if(omsg->getConnection()){
+				if(!omsg->getConnection()->send(omsg)){
+					// Send only fails when connection is closing (or in error state)
+					// This call will free the message
+					omsg->getProtocol()->onSendMessage(omsg);
+				}
+			}
+			else{
+				#ifdef __DEBUG_NET__
+				std::cout << "Error: [OutputMessagePool::send] NULL connection." << std::endl;
+				#endif
+			}
+
+			it = m_autoSendOutputMessages.erase(it);
+		}
+		else{
+			++it;
+		}
+	}
+}
+
+void OutputMessagePool::releaseMessage(OutputMessage* msg)
+{
+	g_dispatcher.addTask(
+		createTask(boost::bind(&OutputMessagePool::internalReleaseMessage, this, msg)));
+}
+
+void OutputMessagePool::internalReleaseMessage(OutputMessage* msg)
+{
+	if(msg->getProtocol()){
+		msg->getProtocol()->unRef();
+#ifdef __DEBUG_NET_DETAIL__
+		std::cout << "Removing reference to protocol " << msg->getProtocol() << std::endl;
+#endif
+	}
+	else{
+		std::cout << "No protocol found." << std::endl;
+	}
+
+	if(msg->getConnection()){
+		msg->getConnection()->unRef();
+#ifdef __DEBUG_NET_DETAIL__
+		std::cout << "Removing reference to connection " << msg->getConnection() << std::endl;
+#endif
+	}
+	else{
+		std::cout << "No connection found." << std::endl;
+	}
+
+	msg->freeMessage();
+	
+	m_outputPoolLock.lock();
+	m_outputMessages.push_back(msg);
+	m_outputPoolLock.unlock();
+}
+
+OutputMessage_ptr OutputMessagePool::getOutputMessage(Protocol* protocol, bool autosend /*= true*/)
+{
+	#ifdef __DEBUG_NET_DETAIL__
+	std::cout << "request output message - auto = " << autosend << std::endl;
+	#endif
+
+	if(!m_isOpen){
+		return OutputMessage_ptr();
+	}
+
+	boost::recursive_mutex::scoped_lock lockClass(m_outputPoolLock);
+
+	if(protocol->getConnection() == NULL){
+		return OutputMessage_ptr();
+	}
+
+	OutputMessage_ptr outputmessage;
+	if(m_outputMessages.empty()) {
+#ifdef __TRACK_NETWORK__
+		if(m_allOutputMessages.size() >= 5000){
+			std::cout << "High usage of outputmessages: " << std::endl;
+			m_allOutputMessages.back()->PrintTrace();
+		}
+#endif
+		outputmessage.reset(new OutputMessage,
+			boost::bind(&OutputMessagePool::releaseMessage, this, _1));
+
+#ifdef __TRACK_NETWORK__
+		m_allOutputMessages.push_back(outputmessage.get());
+#endif
+	} else {
+		outputmessage.reset(m_outputMessages.back(),
+			boost::bind(&OutputMessagePool::releaseMessage, this, _1));
+#ifdef __TRACK_NETWORK__
+		// Print message trace
+		if(outputmessage->getState() != OutputMessage::STATE_FREE) {
+			std::cout << "Using allocated message, message trace:" << std::endl;
+			outputmessage->PrintTrace();
+		}
+#else
+		assert(outputmessage->getState() == OutputMessage::STATE_FREE);
+#endif
+		m_outputMessages.pop_back();
+	}
+
+	configureOutputMessage(outputmessage, protocol, autosend);
+	return outputmessage;
+}
+
+void OutputMessagePool::configureOutputMessage(OutputMessage_ptr msg, Protocol* protocol, bool autosend)
+{
+	msg->Reset();
+	if(autosend){
+		msg->setState(OutputMessage::STATE_ALLOCATED);
+		m_autoSendOutputMessages.push_back(msg);
+	}
+	else{
+		msg->setState(OutputMessage::STATE_ALLOCATED_NO_AUTOSEND);
+	}
+
+	Connection* connection = protocol->getConnection();
+	assert(connection != NULL);
+
+	msg->setProtocol(protocol);
+	protocol->addRef();
+#ifdef __DEBUG_NET_DETAIL__
+	std::cout << "Adding reference to protocol - " << protocol << std::endl;
+#endif
+	msg->setConnection(connection);
+	connection->addRef();
+#ifdef __DEBUG_NET_DETAIL__
+	std::cout << "Adding reference to connection - " << connection << std::endl;
+#endif
+	msg->setFrame(m_frameTime);
+}
