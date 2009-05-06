d7538f0c93c013e69da2eedd7d5f1da7efd66b30
Renan Cunha <renan1bara@gmail.com>
2009-05-06 02:49:11 +0000

    git-svn-id: svn://darghos.com.br/darghos/data/revsvn@692 5021ead5-42e4-5646-a3ec-22fc1710fdc7

--- spells/scripts/support/paralyze_rune.lua
+++ spells/scripts/support/paralyze_rune.lua
@@ -1,10 +1,9 @@
 local combat = createCombatObject()
 setCombatParam(combat, COMBAT_PARAM_EFFECT, CONST_ME_MAGIC_RED)
-setCombatParam(combat, COMBAT_PARAM_TARGETCASTERORTOPMOST, TRUE)
 
 local condition = createConditionObject(CONDITION_PARALYZE)
-setConditionParam(condition, CONDITION_PARAM_TICKS, 60000)
-setConditionFormula(condition, -0.9, 0, -0.9, 0)
+setConditionParam(condition, CONDITION_PARAM_TICKS, 20000)
+setConditionFormula(condition, -1, 40, -1, 40)
 setCombatCondition(combat, condition)
 
 function onCastSpell(cid, var)
