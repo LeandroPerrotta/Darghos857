850f30448d77bb2249aed747ac2392b015d09d6a
Renan Cunha <renan1bara@gmail.com>
2009-05-19 16:13:25 +0000

    git-svn-id: svn://darghos.com.br/darghos/data/revsvn@710 5021ead5-42e4-5646-a3ec-22fc1710fdc7

---
+++ actions/scripts/game/item_nightmare1.lua
@@ -0,0 +1,15 @@
+function onUse(cid, item, fromPosition, itemEx, toPosition)
+	
+
+	if getPlayerStorageValue(cid, sid.FIRST_NIGHTMARE) ~= 1 then
+		doPlayerSendTextMessage(cid, MESSAGE_INFO_DESCR, "Congratulations, now you have the first nightmare addon!")
+		setPlayerStorageValue(cid, sid.FIRST_NIGHTMARE,1)
+		doPlayerAddOutfit(cid, 269,1)
+		doPlayerAddOutfit(cid, 268,1)
+		sendEnvolveEffect(cid, 11)
+	else
+		doPlayerSendTextMessage(cid, MESSAGE_INFO_DESCR, "You already have the first nightmare addon!")		
+	end
+	
+	
+end
