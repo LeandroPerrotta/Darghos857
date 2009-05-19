86255ad44f591fa08641ee041d26014fb5deb4af
Renan Cunha <renan1bara@gmail.com>
2009-05-19 22:31:02 +0000

    git-svn-id: svn://darghos.com.br/darghos/data/revsvn@723 5021ead5-42e4-5646-a3ec-22fc1710fdc7

--- spells/scripts/healing/wound_cleasing.lua
+++ spells/scripts/healing/wound_cleasing.lua
@@ -3,7 +3,6 @@ setCombatParam(combat, COMBAT_PARAM_TYPE, COMBAT_HEALING)
 setCombatParam(combat, COMBAT_PARAM_EFFECT, CONST_ME_MAGIC_BLUE)
 setCombatParam(combat, COMBAT_PARAM_TARGETCASTERORTOPMOST, 1)
 setCombatParam(combat, COMBAT_PARAM_AGGRESSIVE, 0)
-setCombatParam(combat, COMBAT_PARAM_DISPEL, CONDITION_PARALYZE)
 
 function onGetFormulaValues(cid, level, maglevel)
 	local min = ((level/5)+(maglevel*11))
