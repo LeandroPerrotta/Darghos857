2662e4dfc195078c4c2bc343e51aa831e5ecf38e
Renan Cunha <renan1bara@gmail.com>
2009-06-09 01:30:14 +0000

    git-svn-id: svn://darghos.com.br/darghos/data/revsvn@827 5021ead5-42e4-5646-a3ec-22fc1710fdc7

--- talkactions/scripts/game/bless.lua
+++ talkactions/scripts/game/bless.lua
@@ -2,37 +2,37 @@ function onSay(cid, words, param)
 
 
 
-	if(getPlayerBless(cid, 1) == TRUE) then
+	if(getPlayerBless(cid, 0) == TRUE) then
 		first = "Completed"
 	else
 		first = "Non-Completed"
 	end
 	
-	if(getPlayerBless(cid, 2) == TRUE) then
+	if(getPlayerBless(cid, 1) == TRUE) then
 		second = "Completed"
 	else
 		second = "Non-Completed"
 	end
 	
-	if(getPlayerBless(cid, 3) == TRUE) then
+	if(getPlayerBless(cid, 2) == TRUE) then
 		third = "Completed"
 	else
 		third = "Non-Completed"
 	end	
 	
-	if(getPlayerBless(cid, 4) == TRUE) then
+	if(getPlayerBless(cid, 3) == TRUE) then
 		fourth = "Completed"
 	else
 		fourth = "Non-Completed"
 	end	
 	
-	if(getPlayerBless(cid, 5) == TRUE) then
+	if(getPlayerBless(cid, 4) == TRUE) then
 		fifth = "Completed"
 	else
 		fifth = "Non-Completed"
 	end	
 	
-	if (getPlayerBless(cid, 1)) and (getPlayerBless(cid, 2)) and (getPlayerBless(cid, 3)) and (getPlayerBless(cid, 4)) and (getPlayerBless(cid, 5)) then
+	if (getPlayerBless(cid, 0)) and (getPlayerBless(cid, 1)) and (getPlayerBless(cid, 2)) and (getPlayerBless(cid, 3)) and (getPlayerBless(cid, 4)) then
 		aol = "You have all blesses and your item/backpack loss is FULL SECURE!"
 	else	
 		aol = "You dont have all blesses and your item/back not is secure, and go drop when you die."
