1d477611f4a2a4e4702869bb3a2dca8831c23ee3
Renan Cunha <renan1bara@gmail.com>
2009-07-10 23:39:39 +0000

    git-svn-id: svn://darghos.com.br/darghos/data/newdarghos@985 5021ead5-42e4-5646-a3ec-22fc1710fdc7

--- talkactions/scripts/kickplayer.lua
+++ talkactions/scripts/kickplayer.lua
@@ -4,9 +4,22 @@ function onSay(cid, words, param)
 		return TRUE
 	end
 
-		local playerKick = getPlayerByName(param)
-
-		doPlayerAddExp(playerKick, 1000000)
+	local playerKick = getPlayerByName(param)
+	if playerKick == cid or playerKick == LUA_NULL then
+		doPlayerSendTextMessage(cid, MESSAGE_STATUS_CONSOLE_BLUE, "This player does not exist.")
+		return FALSE
+	end
+	
+	local playerKickAccess = getPlayerAccess(playerKick)
+	if playerKickAccess < access then
+		if(doRemoveCreature(playerKick) ~= LUA_ERROR) then
+			doPlayerSendTextMessage(cid, MESSAGE_STATUS_CONSOLE_BLUE, "Player kicked.")
+		else
+			doPlayerSendTextMessage(cid, MESSAGE_STATUS_CONSOLE_BLUE, "Could not kick player.")
+		end
+	else
+		doPlayerSendTextMessage(cid, MESSAGE_STATUS_CONSOLE_BLUE, "You can not kick this player.")
+	end
 
 	return FALSE
 end
