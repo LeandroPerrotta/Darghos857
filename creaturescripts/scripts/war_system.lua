afab16d161bf26a0d83a91526acc0c7a07b24267
Renan Cunha <renan1bara@gmail.com>
2009-09-01 01:58:48 +0000

    git-svn-id: svn://darghos.com.br/darghos/data/newdarghos@1149 5021ead5-42e4-5646-a3ec-22fc1710fdc7

--- creaturescripts/scripts/war_system.lua
+++ creaturescripts/scripts/war_system.lua
@@ -2,11 +2,13 @@ function onKill(cid, target)
 
 	local creature = getCreatureName(target)
 	
-	if(isOfWar(cid) and isOfWar(target)) then
-		doPlayerSendTextMessage(cid, MESSAGE_INFO_DESCR, "Gratz! You murdered player "..creature..".\nBoth guilds are in a state of war, then this death was justified.")
-		doSendMagicEffect(getPlayerPosition(cid), 29)
-		doSendAnimatedText(getPlayerPosition(cid), "WAR FRAG!", TEXTCOLOR_YELLOW)
+	if(isPlayer(cid)) then
+		if(isOfWar(cid) and isOfWar(target)) then
+			doPlayerSendTextMessage(cid, MESSAGE_INFO_DESCR, "Gratz! You murdered player "..creature..".\nBoth guilds are in a state of war, then this death was justified.")
+			doSendMagicEffect(getPlayerPosition(cid), 29)
+			doSendAnimatedText(getPlayerPosition(cid), "WAR FRAG!", TEXTCOLOR_YELLOW)
+		end
 	end
-
+	
 	return LUA_TRUE
 end
