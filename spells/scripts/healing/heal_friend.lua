882065c1e8a9a8c4c050bdae1662c8bdbe5becd1
Leandro Perrotta <leandro_perrotta@hotmail.com>
2010-03-08 23:39:14 +0000

    *adicionado o Warden Set
    git-svn-id: svn://darghos.com.br/darghos/data/newdarghos@1451 5021ead5-42e4-5646-a3ec-22fc1710fdc7

--- spells/scripts/healing/heal_friend.lua
+++ spells/scripts/healing/heal_friend.lua
@@ -7,6 +7,48 @@ setCombatParam(combat, COMBAT_PARAM_DISPEL, CONDITION_PARALYZE)
 function onGetFormulaValues(cid, level, maglevel)
 	local min = ((level/5)+(maglevel*10))
 	local max = ((level/5)+(maglevel*14))
+	
+	local head = getPlayerSlotItem(cid, CONST_SLOT_HEAD)
+	local armor = getPlayerSlotItem(cid, CONST_SLOT_ARMOR)
+	local legs = getPlayerSlotItem(cid, CONST_SLOT_LEGS)
+	local boots = getPlayerSlotItem(cid, CONST_SLOT_FEET)
+	
+	local HELMET_BONUS = {
+		[10555] = 15 -- warden helmet	
+	}
+	
+	local ARMOR_BONUS = {
+		[10556] = 15 -- warden armor
+	}
+	
+	local LEGS_BONUS = {
+		[10557] = 10 -- warden legs
+	}
+	
+	local BOOTS_BONUS = {
+		[10558] = 10 -- warden boots
+	}
+	
+	if(head ~= FALSE and HELMET_BONUS[head.itemid] ~= nil) then
+		min = min * (1 + (HELMET_BONUS[head.itemid] / 100))
+		max = max * (1 + (HELMET_BONUS[head.itemid] / 100))
+	end
+	
+	if(armor ~= FALSE and ARMOR_BONUS[head.itemid] ~= nil) then
+		min = min * (1 + (ARMOR_BONUS[head.itemid] / 100))
+		max = max * (1 + (ARMOR_BONUS[head.itemid] / 100))	
+	end
+	
+	if(legs ~= FALSE and LEGS_BONUS[head.itemid] ~= nil) then
+		min = min * (1 + (LEGS_BONUS[head.itemid] / 100))
+		max = max * (1 + (LEGS_BONUS[head.itemid] / 100))	
+	end
+	
+	if(boots ~= FALSE and BOOTS_BONUS[head.itemid] ~= nil) then
+		min = min * (1 + (BOOTS_BONUS[head.itemid] / 100))
+		max = max * (1 + (BOOTS_BONUS[head.itemid] / 100))	
+	end
+	
 	return min, max
 end
 
