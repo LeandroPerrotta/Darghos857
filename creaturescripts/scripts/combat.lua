a93e04dbbdb77b77223e8fd9545ff9cd2b5931de
Leandro Perrotta <leandro_perrotta@hotmail.com>
2010-05-25 00:55:26 +0000

    *Removido non pvp tiles de IOP *Melhorado onCombat Creature Event
    git-svn-id: svn://darghos.com.br/darghos/data/newdarghos@1553 5021ead5-42e4-5646-a3ec-22fc1710fdc7

--- creaturescripts/scripts/combat.lua
+++ creaturescripts/scripts/combat.lua
@@ -1,7 +1,27 @@
 function onCombat(cid, target)
 
-	if(getPlayerTown(cid) == towns.ISLAND_OF_PEACE and isPlayer(target) == TRUE) then
-		return FALSE
+
+	--checks attacker
+	local player_attacker = nil
+	if(isPlayer(cid) == TRUE) then
+		player_attacker = cid
+	elseif(isPlayer(getCreatureMaster(cid)) == TRUE) then
+		player_attacker = getCreatureMaster(cid)
+	end
+
+	--checks target
+	local player_target = nil
+	if(isPlayer(target) == TRUE) then
+		player_target = target
+	elseif(isPlayer(getCreatureMaster(target)) == TRUE) then
+		player_target = getCreatureMaster(cid)
+	end
+
+	if(player_attacker ~= nil and player_target ~= nil) then
+		if(getPlayerTown(player_attacker) == towns.ISLAND_OF_PEACE or
+		   getPlayerTown(player_target) == towns.ISLAND_OF_PEACE) then
+			return FALSE
+		end
 	end
 
 	return TRUE
