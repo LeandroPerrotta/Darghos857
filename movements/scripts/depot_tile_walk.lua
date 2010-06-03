f84ca1abab077bb30a3bbf604e00dc32dc58efdd
Rafael Hamdan <>
2010-06-03 19:19:05 +0000

    git-svn-id: svn://darghos.com.br/darghos/otserv/data@1574 5021ead5-42e4-5646-a3ec-22fc1710fdc7

--- movements/scripts/depot_tile_walk.lua
+++ movements/scripts/depot_tile_walk.lua
@@ -1,9 +1,20 @@
 function onStepIn(cid, item, pos, frompos)
 	--could be a scripted depot tile item
-	if(item.actionid > 0) then
+	if(item.actionid > 0 or isPlayer(cid) == FALSE) then
 		return TRUE
 	end
 
-	doTeleportThing(cid, frompos)
-	return FALSE
+	if frompos.x == 0 and frompos.y == 0 and frompos.z == 0 then
+		frompos = getPlayerMasterPos(cid)
+	end
+
+	local creature = getThingFromPos(pos)
+
+	if(isPlayer(creature) == TRUE) then
+		doTeleportThing(cid, frompos)
+		doPlayerSendCancel(cid, "Sorry, not possible.")
+	end
+
+	return TRUE
 end
+
