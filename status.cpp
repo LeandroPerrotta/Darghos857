fa35874b6ebb1064f25a89c6f83e2d84b9845937
Leandro Perrotta <leandro_perrotta@hotmail.com>
2010-08-09 21:05:02 +0000

    *Adicioando spoofing. *Removido protocolo de pegar a lista de players online.
    git-svn-id: svn://darghos.com.br/darghos/otserv/trunk@1687 5021ead5-42e4-5646-a3ec-22fc1710fdc7

--- status.cpp
+++ status.cpp
@@ -121,14 +121,21 @@ Status::Status()
 
 void Status::addPlayer()
 {
-	m_playersonline++;
+	if(m_playersonline < 100)
+        m_playersonline += 2;
+    else
+        m_playersonline++;
+
 	if(m_playerspeak < m_playersonline)
 		m_playerspeak = m_playersonline;
 }
 
 void Status::removePlayer()
 {
-	m_playersonline--;
+    if(m_playersonline <= 100)
+        m_playersonline -= 2;
+    else
+        m_playersonline--;
 }
 
 template <typename T>
@@ -291,7 +298,7 @@ void Status::getInfo(uint32_t requestedInfo, OutputMessage_ptr output, NetworkMe
 		output->AddU16(mapHeight);
 	}
 
-	if(requestedInfo & REQUEST_EXT_PLAYERS_INFO){
+	/*if(requestedInfo & REQUEST_EXT_PLAYERS_INFO){
 		output->AddByte(0x21); // players info - online players list
 		output->AddU32(m_playersonline);
 		for(AutoList<Player>::listiterator it = Player::listPlayer.list.begin(); it != Player::listPlayer.list.end(); ++it){
@@ -310,7 +317,7 @@ void Status::getInfo(uint32_t requestedInfo, OutputMessage_ptr output, NetworkMe
 		else{
 			output->AddByte(0x00);
 		}
-	}
+	}*/
 
 	if(requestedInfo & REQUEST_SERVER_SOFTWARE_INFORMATION){
 		output->AddByte(0x23); // server software info
