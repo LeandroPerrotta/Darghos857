67280078afa766df0ca66369832215a0fd033967
Leandro Perrotta <leandro_perrotta@hotmail.com>
2010-11-08 01:44:10 +0000

    *updated
    git-svn-id: svn://darghos.com.br/darghos/otserv/data@1761 5021ead5-42e4-5646-a3ec-22fc1710fdc7

--- creaturescripts/scripts/stages.lua
+++ creaturescripts/scripts/stages.lua
@@ -3,17 +3,6 @@ function onAdvance(cid, type, oldlevel, newlevel)
 	if(type == LEVEL_EXPERIENCE) then
 	
 		setRateStage(cid, newlevel)
-	elseif(type == LEVEL_MAGIC) then
-	
-		if(getPlayerTown(cid) == towns.ISLAND_OF_PEACE and newlevel >= 50) then
-			
-			setSkillRate(cid, type, 1)
-		end
-	else	
-		if(getPlayerTown(cid) == towns.ISLAND_OF_PEACE and newlevel >= 85) then
-		
-			setSkillRate(cid, type, 1)
-		end
 	end		
 	
 	return LUA_TRUE
